0289bd49efd44bcfdee4fd788d72cff3e829bcc1
Carlos Morán <moran_krlos@hotmail.com>
2018-01-20 01:17:13 -0600

    Changes that are part of the chalenge, that consists in add a new page called projects, modify the header including the link and deploying the changes to heroku!

--- server.js
+++ server.js
@@ -60,6 +60,12 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs',{
+        pageTitle: 'My projects!'
+    });
+});
+
 app.listen(port, () =>{
     console.log(`Server is up on port ${port}`);
 });
